514504b5835de01a0f3686f3fbdc9066b8e89d81
Haidar N. <ub.haidarnadhifathaputra@gmail.com>
2025-11-26 21:31:07 +0700

    haloo

--- Assets/Scripts/Day 2/Enemy.cs
+++ Assets/Scripts/Day 2/Enemy.cs
@@ -10,7 +10,8 @@ public class Enemy : MonoBehaviour
 
     public GameObject enemyBulletPrefab;
     Rigidbody2D rb;
-    
+    private Movements player;
+
     void Update()
     {   
         transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
@@ -24,6 +25,7 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {   
+        player = FindObjectOfType<Movements>();
         rb = GetComponent<Rigidbody2D>();
         float yRot = transform.eulerAngles.y;
         if (Mathf.Approximately(Mathf.DeltaAngle(yRot, 180f), 0f) || transform.localScale.x < 0f)
@@ -72,6 +74,8 @@ public class Enemy : MonoBehaviour
     /// </summary>
     public void OnEnemyDestroyed()
     {
+
+        player.killCount += 1;
         // Add score when enemy is destroyed
         if (ScoreManager.Instance != null)
         {
@@ -87,13 +91,19 @@ public class Enemy : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         // If hit by player bullet
-        if (other.CompareTag("PlayerBullet"))
+        if (other.CompareTag("PlayerBullet") || other.CompareTag("Explosion"))
         {
-            // Destroy the bullet
-            Destroy(other.gameObject);
-            
-            // Destroy enemy and add score
-            OnEnemyDestroyed();
+            if (other.CompareTag("Explosion"))
+            {
+                OnEnemyDestroyed();
+            } else
+            {
+                // Destroy the bullet
+                Destroy(other.gameObject);
+                
+                // Destroy enemy and add score
+                OnEnemyDestroyed();
+            }
         }
     }
 
--- Assets/Scripts/Day 2/Movements.cs
+++ Assets/Scripts/Day 2/Movements.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public class Movements : MonoBehaviour
 {
@@ -14,16 +15,32 @@ public class Movements : MonoBehaviour
 
     [Header("Shooting Settings")]
     [SerializeField] private GameObject bulletPrefab;
+    [SerializeField] private GameObject missilePrefab;
+    [SerializeField] private GameObject missilePrefab1;
     [SerializeField] private Transform firePoint;
     [SerializeField] private float bulletSpeed = 15f;
     [SerializeField] private float fireRate = 0.3f;
     [SerializeField] private KeyCode fireKey = KeyCode.Space;
-    
+    [SerializeField] private KeyCode missileKey = KeyCode.E;
+    [SerializeField] private KeyCode missileKey1 = KeyCode.Q;
+    [SerializeField] private int missileMax = 2;
+    private Vector2 lastPos;
+    private Vector2 currentVelocity;
+    public int killCount = 0;
+    private int missileCount = 0;
+
+
     private float nextFireTime = 0f;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        lastPos = rb.position;
+    }
+
+    void Start()
+    {
+        UpdateMissileUI();
     }
 
     void Update()
@@ -35,6 +52,26 @@ public class Movements : MonoBehaviour
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        Vector2 newPos = rb.position;
+        currentVelocity = (newPos - lastPos) / Time.fixedDeltaTime;
+        lastPos = newPos;
+
+        if (missileCount < missileMax)
+        {
+            if (killCount >= 3)
+            {
+                missileCount++;
+                UpdateMissileUI();
+                killCount = 0;
+            }
+        }
+        else
+        {
+            if (killCount > 0)
+            {
+                killCount = 0;
+            }
+        }
     }
 
     /// <summary>
@@ -58,6 +95,12 @@ public class Movements : MonoBehaviour
         {
             Shoot();
             nextFireTime = Time.time + fireRate;
+        } else if (Input.GetKeyDown(missileKey) && missileCount > 0)
+        {
+            ShootMissile();
+        } else if (Input.GetKeyDown(missileKey1) && missileCount > 0)
+        {
+            ShootMissile1();
         }
     }
 
@@ -102,6 +145,56 @@ public class Movements : MonoBehaviour
         bullet.tag = "PlayerBullet";
     }
 
+    private void ShootMissile()
+    {
+        missileCount--;
+        UpdateMissileUI();
+        if (missilePrefab == null)
+        {
+            Debug.LogWarning("Missile prefab is not assigned!");
+            return;
+        }
+        
+        Vector3 spawnPosition = firePoint != null ? firePoint.position : transform.position;
+        
+        spawnPosition.y -= 0.5f;
+        
+        GameObject missile = Instantiate(missilePrefab, spawnPosition, Quaternion.identity);
+        Rigidbody2D missileRb = missile.GetComponent<Rigidbody2D>();
+
+        if (missileRb != null)
+        {
+            missileRb.linearVelocity = new Vector2(currentVelocity.x/2, 0);
+        }
+        
+        missile.tag = "PlayerBullet";
+    }
+
+    private void ShootMissile1()
+    {
+        missileCount--;
+        UpdateMissileUI();
+
+        if (missilePrefab1 == null)
+        {
+            Debug.LogWarning("Missile prefab1 is not assigned!");
+            return;
+        }
+
+        Vector3 spawnPosition = firePoint != null ? firePoint.position : transform.position;
+        spawnPosition.x += 0.7f; // offset
+
+        GameObject missile = Instantiate(missilePrefab1, spawnPosition, Quaternion.identity);
+        Rigidbody2D missileRb = missile.GetComponent<Rigidbody2D>();
+
+        if (missileRb != null)
+        {
+            missileRb.linearVelocity = Vector2.right * (bulletSpeed + 5f); // lebih cepat dari peluru biasa
+        }
+
+        missile.tag = "PlayerBullet";
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.CompareTag("Enemy") || other.CompareTag("EnemyBullet"))
@@ -124,5 +217,12 @@ public class Movements : MonoBehaviour
     private void OnCollisionEnter2D(Collider2D other)
     {
         
+    }
+    private void UpdateMissileUI()
+    {
+        if (ScoreManager.Instance != null)
+        {
+            ScoreManager.Instance.UpdateMissileUI(missileCount, missileMax);
+        }
     }
 }
